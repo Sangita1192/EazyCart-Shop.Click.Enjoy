2854a3668a5813e6dd8ced3f4439b674e389f7ba
Sangita1192 <sangitapanwar64@yahoo.in>
2025-09-13 16:01:27 -0700

    main categories fetch on popularProduct component

--- website/src/Components/HomePage/PopularProducts.jsx
+++ website/src/Components/HomePage/PopularProducts.jsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductItem from '../ProductItem';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';     
-         
- 
+import 'swiper/css';
+import { useSelector } from 'react-redux';
 
 const PopularProducts = () => {
-    const [activeTab, setActiveTab] = useState("Fashion");
 
-    const tabs = ["Fashion", "Electronics", "Bags", "Footwear", "Jewellery", "Beauty", "Wellness"];
+    const [activeTab, setActiveTab] = useState(null);
+    const { categories} = useSelector((state) => state.category);
+
+    useEffect(() => {
+        if (categories.length > 0 && !activeTab) {
+            setActiveTab(categories[0].name);
+        }
+    }, [categories, activeTab]);
+
     return (
         <>
             <div className='bg-[white] md:p-[15px] p-[10px] py-[25px]'>
@@ -16,16 +22,16 @@ const PopularProducts = () => {
                     <div className='flex flex-col lg:flex-row lg:justify-between lg:items-center gap-[20px]'>
                         <h3 className='font-bold text-xl !text-gray-600'>Popular Products</h3>
                         <ul className="flex md:gap-4 gap-2 items-center overflow-x-auto scrollbar-hide lg:ml-[5px] ml-[35px]">
-                            {tabs.map((tab) => (
-                                <li key={tab}>
+                            {categories.length > 0 && categories.map((cat) => (
+                                <li key={cat._id}>
                                     <button
-                                        onClick={() => setActiveTab(tab)}
-                                        className={`cursor-pointer pb-2 px-[15px] transition-all duration-200 font-semibold border-b-2 ${activeTab === tab
+                                        onClick={() => setActiveTab(cat.name)}
+                                        className={`cursor-pointer pb-2 px-[15px] transition-all duration-200 font-semibold border-b-2 ${activeTab == cat.name
                                             ? "border-red-700 text-red-600"
                                             : "border-transparent text-gray-600 hover:text-red-500 hover:border-red-300"
                                             }`}
                                     >
-                                        {tab}
+                                        {cat.name}
                                     </button>
                                 </li>
                             ))}
@@ -33,21 +39,21 @@ const PopularProducts = () => {
                     </div>
                     <div className='py-[10px] my-[15px]'>
                         <Swiper
-                            spaceBetween={15} 
-                            slidesPerView={1} 
+                            spaceBetween={15}
+                            slidesPerView={1}
                             loop={true}
                             breakpoints={{
                                 640: {
-                                    slidesPerView: 2, 
+                                    slidesPerView: 2,
                                 },
                                 768: {
-                                    slidesPerView: 3, 
+                                    slidesPerView: 3,
                                 },
                                 1024: {
-                                    slidesPerView: 4, 
+                                    slidesPerView: 4,
                                 },
                                 1280: {
-                                    slidesPerView: 6, 
+                                    slidesPerView: 6,
                                 },
                             }}
                             className="popular-products-slider"
